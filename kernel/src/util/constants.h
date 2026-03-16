1f483eb98177e22f6e63cf5ecbfb94768755fc84
Armin Nikbakht Tehrani <e11928288@student.tuwien.ac.at>
2026-03-16 14:35:47 +0100

    Remove unused constants

--- kernel/src/util/constants.h
+++ kernel/src/util/constants.h
@@ -2,12 +2,8 @@
 #define _PAMKIT_CONSTANTS_H
 
 #define PROGRAM_NAME "pamkit"
-#define PAMKIT_NAME_TOKEN pamkit
-#define PREPEND_PAMKIT_TOKEN(val) pamkit##_##val
 
 #define PAMKIT_GENERIC_SUCCESS 0
 #define PAMKIT_GENERIC_ERROR -1
-#define PAMKIT_SUCCESS 0
-#define PAMKIT_ERROR 1
 
 #endif /* _PAMKIT_CONSTANTS_H */
